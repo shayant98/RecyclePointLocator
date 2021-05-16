c3ff350713aba2b4229eed394230df5cf95920fb
Shayant Sital <shayant@gmail.com>
2021-05-15 23:49:48 -0300

    modified styles

--- lib/ui/shared/styles.dart
+++ lib/ui/shared/styles.dart
@@ -12,8 +12,8 @@ const Color kShadow = Colors.grey;
 // TextSize
 const double kTitleTextSize = 24;
 const double kSubtitleTextSize = 13;
-const double kBodyTextSize = 14;
-const double kBody2TextSize = 9;
+const double kBodyTextSize = 16;
+const double kBody2TextSize = 12;
 
 // Padding Values
 const double paddingSmall = 15;
@@ -22,21 +22,12 @@ const double paddingRegular = 25;
 const double paddingMedium = 35;
 
 // TextStyle
-const TextStyle kTitleTextStyle = TextStyle(
-    color: kDarkJungleGreen,
-    fontSize: kTitleTextSize,
-    fontWeight: FontWeight.bold);
-const TextStyle kSubtitleTextStyle = TextStyle(
-    color: kOldLavendar,
-    fontSize: kSubtitleTextSize,
-    fontWeight: FontWeight.w500);
-const TextStyle kBodyTextStyle = TextStyle(
-    color: kDarkJungleGreen,
-    fontSize: kBodyTextSize,
-    fontWeight: FontWeight.normal);
-const TextStyle kBody2TextStyle = TextStyle(
-    color: kDarkJungleGreen,
-    fontSize: kBody2TextSize,
-    fontWeight: FontWeight.normal);
-const TextStyle kButtonTextStyle = TextStyle(
-    color: kPlatinum, fontSize: kBodyTextSize, fontWeight: FontWeight.bold);
+const TextStyle kTitleTextStyle =
+    TextStyle(color: kDarkJungleGreen, fontSize: kTitleTextSize, fontWeight: FontWeight.bold);
+const TextStyle kSubtitleTextStyle =
+    TextStyle(color: kOldLavendar, fontSize: kSubtitleTextSize, fontWeight: FontWeight.w500);
+const TextStyle kBodyTextStyle =
+    TextStyle(color: kDarkJungleGreen, fontSize: kBodyTextSize, fontWeight: FontWeight.normal);
+const TextStyle kBody2TextStyle =
+    TextStyle(color: kDarkJungleGreen, fontSize: kBody2TextSize, fontWeight: FontWeight.normal);
+const TextStyle kButtonTextStyle = TextStyle(color: kPlatinum, fontSize: kBodyTextSize, fontWeight: FontWeight.bold);
