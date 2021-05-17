cb30b98665d0b68244b8c58def94c9e4589a4ea7
Shayant Sital <shayant@gmail.com>
2021-05-17 04:16:39 -0300

    removed unused imports

--- lib/ui/detail/detail_viewmodel.dart
+++ lib/ui/detail/detail_viewmodel.dart
@@ -2,16 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:rpl/app/app.locator.dart';
 import 'package:rpl/app/app.router.dart';
-import 'package:rpl/enum/material_type.dart';
 import 'package:rpl/models/application_models.dart';
 import 'package:rpl/service/recycle_point_service.dart';
-import 'package:rpl/service/user_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class DetailViewModel extends BaseViewModel {
   final NavigationService _navigationService = locator<NavigationService>();
-  final UserService _userService = locator<UserService>();
   final RecyclePointService _recyclePointService = locator<RecyclePointService>();
 
   GeoPoint? recyclePointCoordinates;
